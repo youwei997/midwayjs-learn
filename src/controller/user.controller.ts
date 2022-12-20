bc35f09f5001eb4c7febed6a3bc02e84b29a3d93
youwei <1690312648@qq.com>
2022-12-20 18:07:38 +0800

    第三章 4 主键生成器

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,22 +1,22 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Inject,
-  Post,
-  Query,
-} from '@midwayjs/decorator';
+import { Body, Controller, Get, Inject, Post, Query } from '@midwayjs/decorator';
+import { DeleteResult, UpdateResult } from 'typeorm';
+
 import { User } from '../entity/User';
 import { UserService } from '../service/user.service';
-import { DeleteResult, UpdateResult } from 'typeorm';
+import { SnowflakeIdGenerate } from '../utils/Snowflake';
+
 @Controller('/')
 export class HomeController {
   @Inject()
   userService: UserService;
 
+  @Inject()
+  idGenerate: SnowflakeIdGenerate;
+
   @Post('/create')
   async create(@Body() user: User): Promise<User> {
     Object.assign(user, {
+      id: this.idGenerate.generate(),
       regtime: new Date(),
     });
     return this.userService.create(user);
